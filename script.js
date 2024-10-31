d92525f5c4cdb988cb4cf90da0cabef6a92b5675
imckay4 <77985169+imckay4@users.noreply.github.com>
2024-10-31 11:26:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function checkPassword() {
-  const correctPassword = "JPSF@mily";  // Updated password
+  const correctPassword = "JPSF@mily";
   const passwordInput = document.getElementById("password-input").value;
   const errorMessage = document.getElementById("error-message");
-  
+
   if (passwordInput === correctPassword) {
     document.getElementById("password-protect").style.display = "none";
     document.getElementById("main-content").style.display = "block";
